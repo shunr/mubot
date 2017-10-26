cc5dd4001af4ed59ddc1b379a09fe50fa7132135
Shun Yu Rao <syrao@uwaterloo.ca>
2017-10-26 01:52:33 -0400

    Add gitignore and cleanup

--- src/gpio/servo_control.py
+++ src/gpio/servo_control.py
@@ -7,36 +7,36 @@ SERVOS = {}
 PINS_USED = [SERVO_ARM]
 initialized = False
 
-def init():
-  global initialized
-  GPIO.setmode(GPIO.BOARD)
-  GPIO.setup(PINS_USED, GPIO.OUT)
 
-  for pin in PINS_USED:
-      SERVOS[pin] = GPIO.PWM(pin, FREQUENCY)
-      SERVOS[pin].start(0)  # Default duty cycle is 0.0
-  initialized = True
-  print("Initialized GPIO")
+def init():
+    global initialized
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(PINS_USED, GPIO.OUT)
+
+    for pin in PINS_USED:
+        SERVOS[pin] = GPIO.PWM(pin, FREQUENCY)
+        SERVOS[pin].start(0)  # Default duty cycle is 0.0
+    initialized = True
+    print("Initialized GPIO")
+
 
 def _move_servo(servo, duty_cycle):
     global initialized
     print(initialized)
     if not initialized:
-      return
+        return
     SERVOS[servo].ChangeDutyCycle(duty_cycle)
-    print("wew")
+
 
 def move_arm(duty_cycle):
-  _move_servo(SERVO_ARM, duty_cycle)
+    _move_servo(SERVO_ARM, duty_cycle)
 
 
 def cleanup():
     global initialized
     if not initialized:
-      return
+        return
     for servo in SERVOS:
-      SERVOS[servo].stop()
+        SERVOS[servo].stop()
     GPIO.cleanup()
     initialized = False
-
-
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from gpio import servo_control
 
 servo_control.init()
 
+
 def execute(command):
     if command != None:
         print(command)
@@ -15,13 +16,14 @@ def execute(command):
         elif command[0] == "servo":
             print(command)
             if len(command) == 2:
-              servo_control.move_arm(float(command[1]))
+                servo_control.move_arm(float(command[1]))
         elif command[0] == "cleanup":
             servo_control.cleanup()
             sys.exit(0)
 
+
 while 1:
-  #speech_input.start(play)
-  c = input().split()
-  a = (c[0], " ".join(c[1:]))
-  execute(a)
+    # speech_input.start(play)
+    c = input().split()
+    a = (c[0], " ".join(c[1:]))
+    execute(a)
