7867d6e6ef02341f0e14af8d709a6bda758c2fbb
Shun Yu Rao <syrao@uwaterloo.ca>
2017-10-26 00:34:34 -0400

    Add testing from stdin

--- src/main.py
+++ src/main.py
@@ -4,16 +4,24 @@ from subprocess import Popen, PIPE
 import time
 from music import speech_input
 from music import music_player
+from gpio import servo_control
 
-def play(command):
+servo_control.init()
+
+def execute(command):
     if command != None:
         if command[0] == "play":
             music_player.play_from_search(command[1])
         elif command[0] == "stop":
             music_player.stop()
+        elif command[0] == "servo":
+            if len(command) == 2:
+              servo_control.move_arm(int(command[1]))
+        elif command[0] == "cleanup":
+            servo_control.cleanup()
 
 while 1:
   #speech_input.start(play)
   c = input().split()
   a = (c[0], " ".join(c[1:]))
-  play(a)
+  execute(a)
